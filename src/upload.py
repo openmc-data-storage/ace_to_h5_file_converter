c1d582f5706eeb43bc803a31511ef353f7ad7fee
shimwell <mail@jshimwell.com>
2023-10-21 23:40:42 +0100

    move openmc files across

---
+++ src/upload.py
@@ -0,0 +1,22 @@
+from js import document, console
+from pyodide import create_proxy
+from neutron import IncidentNeutron
+# import asyncio
+
+async def _upload_file_and_show(e):
+    console.log("Attempted file upload: " + e.target.value)
+
+    
+    files = e.target.files.to_py()
+    for file in files:
+      file_content = await file.text()
+
+    newDiv = document.createElement('div')
+    newContent = document.createTextNode(f'success {file_content}');
+    newDiv.appendChild(newContent);
+
+    document.getElementById("status_report").appendChild(newDiv)
+
+upload_file = create_proxy(_upload_file_and_show)
+
+document.getElementById("file-upload").addEventListener("change", upload_file)
